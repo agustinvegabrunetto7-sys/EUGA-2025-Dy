99838e89334b0e33b3adba71ca72025728717571
[dyad] <agustinvegabrunetto7@gmail.com>
2025-11-29 17:33:09 -0300

    Updated components/candidates.tsx

--- components/candidates.tsx
+++ components/candidates.tsx
@@ -72,7 +72,7 @@ const teams: Record<string, Array<{ role: string; name: string; bio: string; ima
     { role: "Comisión", name: "Tomas Alzuarena", bio: "", image: "" },
   ],
   edilicias: [
-    { role: "Secretaria", name: "Carolina Villareal", bio: "", image: "" },
+    { role: "Secretaria", name: "Carolina Villareal", bio: "", image: "https://i.postimg.cc/25n1S9Zx/Villareal-Edilicias-Pagina.png" },
     { role: "Sub-secretaria", name: "Ari (4A)", bio: "", image: "" },
     { role: "Comisión", name: "No hay.", bio: "", image: "" },
   ],
